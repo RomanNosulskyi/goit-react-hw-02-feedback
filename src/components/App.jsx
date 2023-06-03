edc88c5e85a2a7c017a7c6136dd3b0943b9ced6e
RomanNosulskyi <nosulskyiroman@gmail.com>
2023-06-03 13:22:04 +0300

    4

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,59 @@
-import { OnFeedback } from './Feedback/Feedback';
-export const App = () => {
-  return (
-    <div
-      style={{
-        display: `block`,
-        padding: '20px',
-      }}
-    >
-      <OnFeedback />
-    </div>
-  );
-};
+import React from 'react';
+import { OnFeedbackOptions } from './Feedback/FeedbackOptions/FeedbackOptions';
+import { OnStats } from '../components/Feedback/Statistics/Statistics';
+import { Notification } from '../components/Feedback/Notification/Notification';
+
+class App extends React.Component {
+  static defaultProps = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+  countTotalFeedback = () => {
+    const total = Object.values(this.state).reduce((previousValue, number) => {
+      return previousValue + number;
+    });
+
+    return total;
+  };
+  countPositiveFeedbackPercentage = () => {
+    const summ = (this.state.good * 100) / this.countTotalFeedback();
+    return Math.round(summ);
+  };
+
+  onLeaveFeedback = evt => {
+    this.setState({ [evt]: this.state[evt] + 1 });
+  };
+  render() {
+    const options = Object.keys(this.state);
+    const { good, neutral, bad } = this.state;
+    return (
+      <>
+        <OnFeedbackOptions
+          options={options}
+          onLeaveFeedback={this.onLeaveFeedback}
+        />
+        {this.countTotalFeedback() !== 0 ? (
+          <OnStats
+            positivePercentage={this.countPositiveFeedbackPercentage()}
+            good={good}
+            bad={bad}
+            neutral={neutral}
+            total={this.countTotalFeedback()}
+          />
+        ) : (
+          <Notification
+            options={options}
+            onLeaveFeedback={this.onLeaveFeedback}
+          />
+        )}
+      </>
+    );
+  }
+}
+export { App };
